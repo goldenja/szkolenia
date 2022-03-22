23ae00c0d96c788fab5c12b9c9165fe93bacd6cd
DESKTOP-LCIR0TI\kbien <k.bieniecki@gmail.com>
2022-03-22 18:14:27 +0100

    fix

--- react/zajecia_react_czesc_14/src/components/00_Sample1.js
+++ react/zajecia_react_czesc_14/src/components/00_Sample1.js
@@ -14,7 +14,6 @@ const reducer = createReducer(initialState, (builder) => {
   builder.addCase(increment, (state, action) => { state.value++ });
 })
 
-
 let store = configureStore({ reducer: reducer })
 
 class Sample1 extends React.Component {
@@ -28,139 +27,11 @@ class Sample1 extends React.Component {
   }
 }
 
-//#4
-// const next = store.dispatch
-// store.dispatch = function dispatchAndLog(action) {
-//   console.log('dispatching', action)
-//   let result = next(action)
-//   console.log('next state', store.getState())
-//   return result
-// }
-
-//#5
-// function logger(store) {
-//   const next = store.dispatch
-  
-//   return function dispatchAndLog(action) {
-//     console.log('dispatching', action.type)
-//     let result = next(action)
-//     console.log('next state', store.getState())
-//     return result
-//   }
-// }
-
-// function logger2(store) {
-//   const next = store.dispatch
-
-//   return function dispatchAndLog(action) {
-//     console.log('dispatching2', action.type)
-//     let result = next(action)
-//     console.log('next state2', store.getState())
-//     return result
-//   }
-// }
-
-// function applyMiddlewareByMonkeypatching(store, middlewares) {
-//   middlewares = middlewares.slice()
-//   middlewares.reverse()
-
-//   middlewares.forEach(middleware => { store.dispatch = middleware(store);} )
-
-    
-// }
-
-// applyMiddlewareByMonkeypatching(store, [logger, logger2]);
-
-
-//#6
-// function logger(store) {
-//   return function wrapDispatchToAddLogging(next) {
-//     return function dispatchAndLog(action) {
-//       console.log('dispatching', action.type)
-//       let result = next(action)
-//       console.log('next state', store.getState())
-//       return result
-//     }
-//   }
-// }
-
-// const logger2 = store => next => action => {
-//   console.log('dispatching2', action)
-//   let result = next(action)
-//   console.log('next state2', store.getState())
-//   return result
-// }
-
-// function applyMiddleware(store, middlewares) {
-//   middlewares = middlewares.slice()
-//   middlewares.reverse()
-//   let dispatch = store.dispatch
-//   middlewares.forEach(middleware => (dispatch = middleware(store)(dispatch)))
-
-
-//   return Object.assign({}, store, { dispatch })
-// }
-
-
-// store = applyMiddleware(store, [logger, logger2]);
-
-//#7
-
-// function logger(store) {
-//   return function wrapDispatchToAddLogging(next) {
-//     return function dispatchAndLog(action) {
-//       console.log('dispatching', action.type)
-//       let result = next(action)
-//       console.log('next state', store.getState())
-//       return result
-//     }
-//   }
-// }
-
-// function logger2(store) {
-//   return function wrapDispatchToAddLogging(next) {
-//     return function dispatchAndLog(action) {
-//       console.log('dispatching2', action.type)
-//       let result = next(action)
-//       console.log('next state2', store.getState())
-//       return result
-//     }
-//   }
-// }
-
-//  let store = createStore(reducer , applyMiddleware(logger, logger2));
-
-
 function Counter() {
   const count = useSelector((state) => state.value)
   const dispatch = useDispatch()
 
-  //#1
    const clickEvent = () => dispatch(increment());
-
-  //#2
-  // const clickEvent = () => { 
-
-  //   console.log('dispatching', increment.type);
-  //   dispatch(increment()) 
-  //   console.log('next state', store.getState())
-  // };
-
-  //#3
-  //  const clickEvent = () => { 
-  //   dispatchAndLog (store,increment());
-  // };
-
-  // function dispatchAndLog(storeParam, action) {
-  //   console.log('dispatching', action.type)
-  //   storeParam.dispatch(action)
-  //   console.log('next state', storeParam.getState())
-  // }
-
-  //#4 , #5, #6 , #7 ... / patrz góra
-  // const clickEvent = () => dispatch(increment());
-
-
 
   return (
     <div>
@@ -175,8 +46,6 @@ function Counter() {
     </div>
   )
 }
-
-
 
 
 
